a1ee7d034efb529b0e0a7250624a7d67ead1395a
Your Name <githakawendowanjiru@gmail.com>
2022-07-05 11:46:20 -0700

    g

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * main - Entry point
- * Return:0
- */
-
+* main - Prints main
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-char c[10] = "Holberton";
-int i = 0;
-while (i < 9)
-{
-_putchar(c[i]);
-i++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
