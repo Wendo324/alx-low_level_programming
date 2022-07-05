453ad8ffd7d4d0cee872cc24e18263f1e694021f
Your Name <githakawendowanjiru@gmail.com>
2022-07-05 15:01:00 -0700

    d

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,90 +1,50 @@
 #include "main.h"
 
-
-
-int _putchar(char c);
-
-
-
 /**
-* print_times_table - print the times table until n
-*
-* @n: n times table
-*
-* Return: No return
+* print_times_table - prints times table
+* @n : times table to use
+* Return:void
 */
-
-
 
 void print_times_table(int n)
 {
-int i;
-int j;
 
+int a = 0, rep, b;
 
+if (n < 0 || n > 15)
+return;
 
-if (n >= 0 && n <= 15)
+while (a <= n)
 {
-
-
-
-for (i = 0; i <= n; i++)
+for (b = 0; b <= n; b++)
 {
-
-
-
-for (j = 0; j <= n; j++)
-{
-
-
-
-if ((i * j) < 10)
-{
-if (j != 0
+rep = a * b;
+if (b == 0)
+_putchar('0' + rep);
+else if (rep < 10)
 {
 _putchar(' ');
 _putchar(' ');
-_putchar(' ');
+_putchar('0' + rep);
 }
-_putchar((j * i) + '0');
+else if (rep < 100)
+{_putchar(' ');
+_putchar('0' + rep / 10);
+_putchar('0' + rep % 10);
 }
-
-
-
-else if ((i * j) < 100)
+else
 {
-_putchar(' ');
-_putchar(' ');
-_putchar((((j * i) / 10) % 10) + '0');
-_putchar(((j * i) % 10) + '0');
+_putchar('0' + rep / 100);
+_putchar('0' + (rep - 100) / 10);
+_putchar('0' + rep % 10);
 }
-
-
-
-else if ((i * j) < 1000)
-{
-_putchar(' ');
-_putchar((((j * i) / 100) % 10) + '0');
-_putchar((((j * i) / 10) % 10) + '0');
-_putchar(((j * i) % 10) + '0');
-}
-if (j != (n))
+if (b < n)
 {
 _putchar(',');
+_putchar(' ');
 }
-
-
-
 }
-
-
-_putchar('\n')
+_putchar('\n');
+a++;
 }
-
-
 }
-
-
-
-}
-
