2e5d1a438114cb01c0ffb933e88d1c26597cad62
Your Name <githakawendowanjiru@gmail.com>
2022-07-11 04:18:52 -0700

    d

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,36 @@
-#include "main.h"
+include "main.h"
 
 /**
-* _atoi - convert a string to an integer.
-* @s: char type string
-* Return: integer converted
+* _atoi - Convert a string to integer.
+* @s: char array string
+* Return: first integer found in string
 */
 
 int _atoi(char *s)
 {
-int sign = 1, resp = 0, firstNum;
+int i;
+int h, p;
 
-for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+h = 0;
+p = -1;
+for (i = 0; s[i] != '\0'; i++)
 {
-if (s[firstNum] == '-')
+if (s[i] == '-')
+p *= -1;
+
+if (s[i] > 47 && s[i] < 58)
 {
-sign *= -1;
+if (h < 0)
+h = (h * 10) - (s[i] - '0');
+else
+h = (s[i] - '0') * -1;
+
+if (s[i + 1] < 48 || s[i + 1] > 57)
+break;
 }
 }
+if (p < 0)
+h *= -1;
 
-for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-{
-resp *= 10;
-resp += (s[i] - 48);
-}
-
-return (sign * resp);
+return (h);
 }
