93215e5e03f2ed464298d27d54d6fde2146c04ed
Your Name <githakawendowanjiru@gmail.com>
2022-07-18 23:32:18 -0700

    t6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
-* print_chessboard - print chessboard
-* @a: input chars
-*
-* Description: print_chessboard
-* Returns: always 0
+* print_chessboard - prints chessboard.
+* @a: matrix
 */
 
 void print_chessboard(char (*a)[8])
@@ -16,7 +13,7 @@ for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
 {
-_putchar(a[i][j]);
+_putchar(*(*(i + a) + j));
 }
 _putchar('\n');
 }
