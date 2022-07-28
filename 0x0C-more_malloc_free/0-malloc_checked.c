975e8b808c752c64c8d81d47d5203c15b42d6b36
Your Name <githakawendowanjiru@gmail.com>
2022-07-27 20:51:17 -0700

    task3

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- *malloc_checked - array for prints a string
- *@b: number of memory
- *Return: void
- */
+*malloc_checked - array for prints a string
+*@b: number of memory
+*Return: void
+*/
 
 void *malloc_checked(unsigned int b)
 {
-  void *p;
+void *p;
 
-  p = malloc(b);
-  if (p == NULL)
-    {
-      exit(98);
-    }
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
 
-  return (p);
+return (p);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- *_calloc -allocated memoria for nmeb elemn de zise bytes
- *@nmemb: number of element in the array
- *@size: bytes for each position in array
- *Return: pointer void
- */
+*_calloc -allocated memoria for nmeb elemn de zise bytes
+*@nmemb: number of element in the array
+*@size: bytes for each position in array
+*Return: pointer void
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  char *p;
-  unsigned int i;
+char *p;
+unsigned int i;
 
-  if (nmemb == 0 || size == 0)
-    return (NULL);
+if (nmemb == 0 || size == 0)
+return (NULL);
 
-  p = malloc(nmemb * size);
-  if (p == NULL)
-    return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
 
-  for (i = 0; i < nmemb * size; i++)
-    p[i] = 0;
+for (i = 0; i < nmemb * size; i++)
+p[i] = 0;
 
-  return (p);
+return (p);
 
 }
